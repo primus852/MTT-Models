19947459b59da73025a1db9fa59ffec3ee5f937f
Torte <tow.berlin@gmail.com>
2021-07-06 18:28:21 +0200

    Comment

--- src/mtt/short.py
+++ src/mtt/short.py
@@ -43,17 +43,18 @@ class MTTShort:
                                     patience=1,
                                     restore_best_weights=True), cp_callback])
 
-            tf.saved_model.save(m, checkpoint_path)
-            loaded = tf.saved_model.load(checkpoint_path)
-            infer = loaded.signatures['serving_default']
-
-            f = tf.function(infer).get_concrete_function(flatten_input=tf.TensorSpec(shape=[None, 28, 28, 1], dtype=tf.float32))
-            f2 = convert_variables_to_constants_v2(f)
-            graph_def = f2.graph.as_graph_def()
-
-            # Export frozen graph
-            with tf.io.gfile.GFile('frozen_graph.pb', 'wb') as f:
-                f.write(graph_def.SerializeToString())
+            # TODO: SAVE TO OPENCV COMPATIBLE FILE
+            # tf.saved_model.save(m, checkpoint_path)
+            # loaded = tf.saved_model.load(checkpoint_path)
+            # infer = loaded.signatures['serving_default']
+            #
+            # f = tf.function(infer).get_concrete_function(flatten_input=tf.TensorSpec(shape=[None, 28, 28, 1], dtype=tf.float32))
+            # f2 = convert_variables_to_constants_v2(f)
+            # graph_def = f2.graph.as_graph_def()
+            #
+            # # Export frozen graph
+            # with tf.io.gfile.GFile('frozen_graph.pb', 'wb') as f:
+            #     f.write(graph_def.SerializeToString())
 
             # Plot Comparison
             Helper.plot_loss_val_accuracy_comparison(history, model_name)
